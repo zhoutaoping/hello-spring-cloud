0e279afbff2a3c1991fb581ad1a47de41268aece
zhoutaoping <18297604236@163.com>
2019-02-20 16:36:07 +0800

    first commit

---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/ztp/hello/spring/cloud/web/admin/feign/service/hystrix/AdminServiceHystrix.java
@@ -0,0 +1,13 @@
+package com.ztp.hello.spring.cloud.web.admin.feign.service.hystrix;
+
+import com.ztp.hello.spring.cloud.web.admin.feign.service.AdminService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminServiceHystrix implements AdminService {
+
+    @Override
+    public String sayHi(String message) {
+        return "Hi，your message is :\"" + message + "\" but request error feign.";
+    }
+}
---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/ztp/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -0,0 +1,24 @@
+package com.ztp.hello.spring.cloud.web.admin.ribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String sayHi(String message) {
+        return restTemplate.getForObject("http://HELLO-SPRING-CLOUD-SERVICE-ADMIN/hi?message=" + message, String.class);
+    }
+    
+    public String hiError(String message) {
+        return "Hi，your message is :\"" + message + "\" but request error.";
+    }
+    
+}
